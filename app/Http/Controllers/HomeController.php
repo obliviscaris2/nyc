a8f5e430ffd33c3af8f449a652d4ef1e7aa79929
finchmaester <finchbibek@outlook.com>
2022-12-18 11:57:37 +0545

    Home COntroller added and Sitesetting seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\Post;
+use App\Models\Team;
+use App\Models\About;
 use App\Models\Image;
+use App\Models\Video;
+use App\Models\MyImage;
+use App\Models\SiteSetting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,10 +19,22 @@ class HomeController extends Controller
     //
     public function index(Request $req){
 
-        $link = Link::first()->get()->take(5);
+        $links = Link::latest()->get()->take(5);
+        $images = MyImage::latest()->get()->take(5);
+        $teams = Team::latest()->get()->take(5);
+        $abouts = About::latest()->get()->take(1);
+        $videos = Video::latest()->get()->take(3);
+        $posts = Post::latest()->get()->take(6);
+        $sitesetting = SiteSetting::first();
 
         return view('portal.index', [
-            "link" => $link,
+            'links' => $links,
+            'images' => $images,
+            'teams' => $teams,
+            'abouts' => $abouts,
+            'videos' => $videos,
+            'posts' => $posts,
+            'sitesetting' => $sitesetting,
             
         ]);
         
