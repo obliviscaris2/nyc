e738c781d58e0028773670ca493faf5576bb641e
@neobliviscaris2 <ergasiamellow@gmail.com>
2022-12-28 12:19:59 +0545

    fixed-carousel

--- resources/views/portal/includes/coverImage.blade.php
+++ resources/views/portal/includes/coverImage.blade.php
@@ -1,5 +1,6 @@
 @php
 $images = App\Models\CoverImage::all();
+$img = App\Models\CoverImage::first();
 @endphp
     
     {{-- For Cover Image --}}
@@ -9,12 +10,16 @@ $images = App\Models\CoverImage::all();
               <div class="col-md-9 cover_slider">
                   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <img src="{{ asset('uploads/coverimage/' . $img->image) }}" class="d-block" alt="...">
+                        </div>
                           @foreach ($images as $image )
                               
+                            <div class="carousel-item">
+                                <img src="{{ asset('uploads/coverimage/' . $image->image) }}" class="d-block" alt="...">
+                            </div>
                           
-                          <div class="carousel-item active">
-                              <img src="{{ asset('uploads/coverimage/' . $image->image) }}" class="d-block" alt="...">
-                          </div>
 
                           @endforeach
                         
@@ -23,16 +28,7 @@ $images = App\Models\CoverImage::all();
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                       </button>
-                      {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                          data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                      </button> --}}
-                      {{-- <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                          data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                      </button> --}}
+
                       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
