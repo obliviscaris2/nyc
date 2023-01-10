4ea395ab2e70c81e8825e1151c08409a016b3b59
obliviscaris2 <93909914+obliviscaris2@users.noreply.github.com>
2023-01-10 11:41:52 +0545

    Update DatabaseSeeder.php
    removed second sitesettingseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TeamSeeder::class);
         $this->call(SiteSettingSeeder::class);
         
-        $this->call(SiteSettingSeeder::class);
 
     }
 }
