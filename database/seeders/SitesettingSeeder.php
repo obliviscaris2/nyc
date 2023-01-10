9e39d75be5625c27262bbcbd2f82bc742bd7b55a
@neobliviscaris2 <ergasiamellow@gmail.com>
2023-01-10 11:36:48 +0545

    sitesetting-seeder

--- database/seeders/SitesettingSeeder.php
+++ database/seeders/SitesettingSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\SiteSetting;
 use Illuminate\Database\Seeder;
 
-class SitesettingSeeder extends Seeder
+class SiteSettingSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,19 +15,19 @@ class SitesettingSeeder extends Seeder
     public function run()
     {
         SiteSetting::create([
-            'govn_name' => 'Nepal Government',
-            'ministry_name' => 'Ministry Name',
-            'department_name' => 'Department Name',
-            'office_name' => 'Office Name',
-            'office_address' => 'Office Address',
-            'office_contact' => 'Office Contact',
-            'office_mail' => 'officemail@gmail.com',
-            'main_logo' => '',
-            'side_logo' => '',
-            'face_link' => 'https://www.facebook.com/',
+            'govn_name' => 'Government of Nepal',
+            'ministry_name' => 'Ministry of Youth and Sports',
+            'department_name' => 'Ministry of Youth and Sports',
+            'office_name' => 'National Youth Council',
+            'office_address' => 'Sanothimi, Bhaktapur',
+            'office_contact' => '6635133',
+            'office_mail' => 'info@nyc.gov.np',
+            'main_logo' => 'uploads/sitesetting/main_logo.png',
+            'side_logo' => 'uploads/sitesetting/side_logo.jpg',
+            'flag_logo' => 'uploads/sitesetting/flag_logo.gif',
+            'face_link' => 'https://www.facebook.com/nationalyouthcouncilnepal/',
             'insta_link' => 'https://www.instagram.com/',
             'social_link' => 'https://www.social.com/',
         ]);
-
     }
 }
