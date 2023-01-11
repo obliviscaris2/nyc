2f5049f189ac29225389272d0fdec0a10cfc1b2f
NIharikaGhimire <niharikaghimire29@gmail.com>
2023-01-11 14:31:38 +0545

    facebook_plugin added on home page

--- resources/views/portal/includes/news.blade.php
+++ resources/views/portal/includes/news.blade.php
@@ -1,10 +1,4 @@
 @php
-<<<<<<< HEAD
-=======
-    
-    $notice = App\Models\Document::whereType('notice')->latest()->get()->take(5);
-    $news = App\Models\Other::whereType('news')->latest()->get()->take(5);
->>>>>>> 487009eca607b34cf2cf4e13a492f6ebfdd3d327
 
 $notices = App\Models\Document::whereType('notice')->latest()->get()->take(15);
 // $news = App\Models\Other::whereType('news')->latest()->get()->take(15);
@@ -14,7 +8,6 @@ $notices = App\Models\Document::whereType('notice')->latest()->get()->take(15);
 @endphp
 
 
-<<<<<<< HEAD
 
 
 {{-- For Quick News --}}
@@ -36,28 +29,6 @@ $notices = App\Models\Document::whereType('notice')->latest()->get()->take(15);
 
                         <p>
                             {{ $notice->title }}
-=======
-                <button class="qn_button">
-                    {{ __("Highlights") }}
-                </button>
-                <div class="news_track">
-
-                    @foreach ($data as $data )
-                        
-                  
-                    <div class="news_slide">
-                        {{-- <a href="{{ route('render_otherpost', $data->slug)}}"> --}}
-  
-                            <p>
-                                        {{ $data->title }}
-                                
-                            </p>
-                        {{-- </a> --}}
-                       
-                    </div>
-                    @endforeach
-                 
->>>>>>> 487009eca607b34cf2cf4e13a492f6ebfdd3d327
 
 
 
