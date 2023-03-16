cb20c5e0479d9c0ec1c5e8a7e0aff77cb45dfc0c
finchmaester <finchbibek@outlook.com>
2023-03-16 10:51:46 +0545

    changes in the front end

--- resources/views/admin/coverimage/index.blade.php
+++ resources/views/admin/coverimage/index.blade.php
@@ -1,32 +1,24 @@
-@extends('admin.master')
- 
- 
-  @section('content') 
-  @if(session('successMessage'))
-<div class="alert alert-success">
-  {!! session('successMessage') !!}
-</div>
-@endif
+@extends('admin.layouts.master')
 
-@if(session('error'))
-<div class="alert alert-danger">
-  {!! session('error') !!}
-</div>
-@endif
-   
-   
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0">{{ $page_title }}</h1>
-             <a href="{{ url('admin/coverimage/create') }}"><button class="btn-primary btn-sm"><i class="fa fa-plus"></i>Add Cover Image</button></a> 
-            </div>
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ url('admin') }}">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard v1</li>
-              </ol>
-            </div>
-          </div>
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+
+    <!-- Content Header (Page header) -->
+
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ $page_title }}</h1>
+                    <a href="{{ route('admin.coverimage.create') }}"><button class="btn-primary btn-sm"><i class="fa fa-plus"></i>
+                            Add New</button></a>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ url('admin') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
        
   
     
